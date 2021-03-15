3d11703961ea9eeb77493e6e2bee826e63590720
Patrick Moss <patrickmoss20@gmail.com>
2021-03-14 19:29:07 -0700

    fix: Passed tests

--- main.py
+++ main.py
@@ -2,22 +2,25 @@
 # -*- coding: utf-8 -*-
 from app.translator import XTranslator
 import os
-#Tester
+
+# Tester
 
 
-#Find all docx files
-docsToTranslate = []  
+# Find all docx files
+docsToTranslate = []
 
 
-#Find all docx in folder to translate
-for path, subdirs, files in os.walk("app/docsToTranslate/"): 
+# Find all docx in folder to translate
+for path, subdirs, files in os.walk("app/docsToTranslate/"):
     for name in files:
         if os.path.splitext(os.path.join(path, name))[1] == ".docx":
             docsToTranslate.append(name)
 
 for file in docsToTranslate:
-    XTranslator('docsToTranslate/'+file,"docsToTranslate/translatedDocs/TRANSLATED_"+file)
+    XTranslator(
+        "docsToTranslate/" + file, "docsToTranslate/translatedDocs/TRANSLATED_" + file
+    )
 
-#Todo fix path for translation input and output relative to translator.py
+# Todo fix path for translation input and output relative to translator.py
 
-
+# Test on precommit
